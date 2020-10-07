e8d540a44c5723bf5574090757f9d7c176dacfd1
unknown <tongsilin@corp.netease.com>
2020-10-07 22:20:13 +0800

    爬取数据

--- pycbg.py
+++ pycbg.py
@@ -7,4 +7,10 @@ if len(sys.argv) > 1 :
 	mapAddress = ''.join(sys.argv[1:])
 else :
 	mapAddress = pyperclip.paste()
-webbrowser.open('https://n.cbg.163.com/cbg/query.py?serverid=5&act=search_role'+mapAddress)
+#webbrowser.open('https://n.cbg.163.com/cbg/query.py?serverid=5&act=search_role'+mapAddress)
+import requests
+
+if __name__ == '__main__':
+	target = 'https://n.cbg.163.com/cbg/query.py?serverid=5&order=total_score+DESC&act=search_role'
+	req = requests.get(url=target)
+	print(req.text)
