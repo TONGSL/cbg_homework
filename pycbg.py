b6be0689f7b641289703e917fe75163ef6c3c64e
unknown <tongsilin@corp.netease.com>
2020-10-07 23:22:59 +0800

    提取账号数据部分

--- pycbg.py
+++ pycbg.py
@@ -9,8 +9,12 @@ else :
 	mapAddress = pyperclip.paste()
 #webbrowser.open('https://n.cbg.163.com/cbg/query.py?serverid=5&act=search_role'+mapAddress)
 import requests
-
+from bs4 import BeautifulSoup
 if __name__ == '__main__':
 	target = 'https://n.cbg.163.com/cbg/query.py?serverid=5&order=total_score+DESC&act=search_role'
 	req = requests.get(url=target)
-	print(req.text)
+	rawhtml = req.text
+	bf = BeautifulSoup(rawhtml,"html.parser")
+	texts = bf.find_all('table',cellspacing ='0')
+	#texts = bf.find_all('table','date-serverid' =='5')
+	print(texts)
