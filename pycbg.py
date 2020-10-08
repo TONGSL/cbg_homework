34b07ec8299fe2b8abb0dee210f8fd8c5783d97e
unknown <tongsilin@corp.netease.com>
2020-10-08 23:06:36 +0800

    正则账号数据

--- pycbg.py
+++ pycbg.py
@@ -10,11 +10,40 @@ else :
 #webbrowser.open('https://n.cbg.163.com/cbg/query.py?serverid=5&act=search_role'+mapAddress)
 import requests
 from bs4 import BeautifulSoup
-if __name__ == '__main__':
-	target = 'https://n.cbg.163.com/cbg/query.py?serverid=5&order=total_score+DESC&act=search_role'
+import re
+
+def getContent(target):
+# 获取爬取网页的内容
 	req = requests.get(url=target)
 	rawhtml = req.text
 	bf = BeautifulSoup(rawhtml,"html.parser")
 	texts = bf.find_all('table',cellspacing ='0')
 	#texts = bf.find_all('table','date-serverid' =='5')
-	print(texts)
+	#print(texts)
+	return texts
+
+def parseContent(texts):
+# 正则需要的账号信息
+	pattern = re.compile('<img.*?alt="(.*?)".*?总评分.*?"val">(.*?)</span>.*?修为.*?"val">(.*?)</span>.*?修炼.*?"val">(.*?)</span>.*?装备评分.*?"val">(.*?)</span>.*?基础评分.*?"val">(.*?)</span>.*?"n-highlights".*?<i>(.*?)</td>.*?<td>(.*?)</td>.*?<td>.*?<span>(.*?)</span>.*?mapPriceColor[(](.*?)[)][)]</script>',re.S)
+	items = re.findall(pattern,str(texts))
+	print(items)
+
+def main(page):
+	if page == 1 :
+		page = ''
+	else :
+		page = 'page='+str(page)
+	target = 'https://n.cbg.163.com/cbg/query.py?serverid=5&'+page+'&order=total_score+DESC&act=search_role'
+	texts = getContent(target)
+	parseContent(texts)
+
+
+if __name__ == '__main__':
+	for i in range(1):
+		main(i)
+	
+	
+	
+	
+	
+
